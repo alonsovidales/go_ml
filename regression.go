63f36cf9e4ca4f48ae9d736d702dad77678296cb
Alonso Vidales <alonso.vidales@tras2.es>
2013-12-01 23:31:18 +0100

    Added a method to load the data set from a text file

--- regression.go
+++ regression.go
@@ -3,39 +3,44 @@ package ml
 import (
     "fmt"
     "math"
+    "io/ioutil"
+    "strings"
+    "strconv"
 )
 
 type DataSet struct {
-    x [][]float64 // Training set of values for each feature
-    y []float64 // The training set with values to be predicted
+    X [][]float64 // Training set of values for each feature
+    Y []float64 // The training set with values to be predicted
 }
 
+// Calculates the cost function and gradient for the data with the given theta
+// and lambda 
 func (data *DataSet) LinearRegCostFunction(theta []float64, lambda float64) (j float64, grad []float64, err error) {
-    if len(data.y) != len(data.x) {
+    if len(data.Y) != len(data.X) {
         err = fmt.Errorf(
-	    "The number of test cases (X) %d doesn't corresponds with the number of values (Y) %d",
-	    len(data.x),
-	    len(data.y))
-	return
+            "The number of test cases (X) %d doesn't corresponds with the number of values (Y) %d",
+            len(data.X),
+            len(data.Y))
+        return
     }
 
-    if len(theta) != len(data.x[0]) {
+    if len(theta) != len(data.X[0]) {
         err = fmt.Errorf(
-	    "The Theta arg has a lenght of %d and the input data %d",
-	    len(theta),
-	    len(data.x[0]))
-	return
+            "The Theta arg has a lenght of %d and the input data %d",
+            len(theta),
+            len(data.X[0]))
+        return
     }
 
-    m := len(data.x)
+    m := len(data.X)
     mF := float64(m)
     // Calculate the hipotesis for each training val
     hipotesis := make([]float64, m)
 
     for i := 0; i < m; i++ {
         for j:= 0; j < len(theta); j++ {
-	    hipotesis[i] += data.x[i][j] * theta[j]
-	}
+            hipotesis[i] += data.X[i][j] * theta[j]
+        }
     }
 
     cost := 0.0
@@ -44,16 +49,16 @@ func (data *DataSet) LinearRegCostFunction(theta []float64, lambda float64) (j f
     // Calculate the sum of the square of distances between the wanted result
     // and the actual one, the total cost
     for i := 0; i < len(hipotesis); i++ {
-	cost += math.Pow(hipotesis[i] - data.y[i], 2)
+        cost += math.Pow(hipotesis[i] - data.Y[i], 2)
 
         for j:= 0; j < len(theta); j++ {
-	    grad[j] += (hipotesis[i] - data.y[i]) * data.x[i][j]
-	}
+            grad[j] += (hipotesis[i] - data.Y[i]) * data.X[i][j]
+        }
     }
 
     thetaReg := 0.0
     for i := 1; i < len(theta); i++ {
-	thetaReg += theta[i]
+        thetaReg += theta[i]
     }
 
     // Calculate the regularized cost
@@ -61,7 +66,40 @@ func (data *DataSet) LinearRegCostFunction(theta []float64, lambda float64) (j f
 
     grad[0] /= mF
     for j:= 1; j < len(theta); j++ {
-	grad[j] = (grad[j] / mF) + ((lambda * theta[j]) / mF)
+        grad[j] = (grad[j] / mF) + ((lambda * theta[j]) / mF)
+    }
+
+    return
+}
+
+// Loads information from the local file located at filePath, and after parse
+// it, returns the DataSet ready to be used with all the information loaded
+// The file format is:
+//      X11 X12 ... X1N Y1
+//      X21 X22 ... X2N Y2
+//      ... ... ... ... ..
+//      XN1 XN2 ... XNN YN
+//
+// Note: Use a single space as separator
+func LoadFile(filePath string) (data *DataSet) {
+    strInfo, err := ioutil.ReadFile(filePath)
+    if err != nil { panic(err) }
+    data = new(DataSet)
+
+    trainingData := strings.Split(string(strInfo), "\n")
+    for _, line := range trainingData {
+        if line == "" {
+            break
+        }
+
+        var values []float64
+        for _, value := range strings.Split(line, " ") {
+            floatVal, err := strconv.ParseFloat(value, 64)
+            if err != nil { panic(err) }
+            values = append(values, floatVal)
+        }
+        data.X = append(data.X, values[:len(values) - 1])
+        data.Y = append(data.Y, values[len(values) - 1])
     }
 
     return
--- regression_test.go
+++ regression_test.go
@@ -2,72 +2,52 @@ package ml
 
 import (
     "testing"
-    "fmt"
 )
 
 func TestLinearRegCostFunction(t *testing.T) {
 
     // Data obtained from the "Andrew Ng" machine learning course from Coursera
     // https://www.coursera.org/course/ml
-    x := [][]float64 {
-	[]float64{1, -15.9368},
-	[]float64{1, -29.1530},
-	[]float64{1, 36.1895},
-	[]float64{1, 37.4922},
-	[]float64{1, -48.0588},
-	[]float64{1, -8.9415},
-	[]float64{1, 15.3078},
-	[]float64{1, -34.7063},
-	[]float64{1, 1.3892},
-	[]float64{1, -44.3838},
-	[]float64{1, 7.0135},
-	[]float64{1, 22.7627},
+    data := LoadFile("test_data/test_logistic.dat")
+
+    theta := [][]float64 {
+        []float64{1, 1},
+        []float64{13.08790, 0.36778},
+    }
+    lambda := []float64 {
+        1,
+        0,
     }
 
-    y := []float64{
-	2.1343,
-	1.1733,
-	34.3591,
-	36.8380,
-	2.8090,
-	2.1211,
-	14.7103,
-	2.6142,
-	3.7402,
-	3.7317,
-	7.6277,
-	22.7524,
+    expectedJ := []float64 {
+        303.9937949908333,
+        22.3738964254415,
     }
 
-    expectedJ := 303.9937949908333
-    expectedGrad := [2]float64{-15.303049999999999, 598.251293035}
-
-    data := &DataSet{
-	x: x[:],
-	y: y,
+    expectedGrad := [][]float64 {
+        []float64{-15.303049999999999, 598.251293035},
+        []float64{-3.2069500000601416e-05, 0.0009341162152152194},
     }
 
-    j, grad, err := data.LinearRegCostFunction([]float64{1, 1}, 1)
+    for test := 0; test < len(expectedGrad); test++ {
+        j, grad, err := data.LinearRegCostFunction(theta[test], lambda[test])
 
-    // The expected results are:
-    //	J: 303.99
-    //	Grad: [-15.303, 598.251]
+        // The expected results are:
+        //        J: 303.99
+        //        Grad: [-15.303, 598.251]
 
-    if err != nil {
-	t.Error("The LinearRegCostFunction returned an error:", err)
+        if err != nil {
+            t.Error("The LinearRegCostFunction returned an error:", err)
+        }
+
+        if (j != expectedJ[test]) {
+            t.Error("The expected value for J is:", expectedJ[test], "but the returned value is:", j)
+        }
+
+        for i := 0; i < len(grad); i++ {
+            if (grad[i] != expectedGrad[test][i]) {
+                t.Error("The expected gradient is:", expectedGrad[test][i], "but the returned value is:", grad[i])
+            }
+        }
     }
-
-    if (j != expectedJ) {
-	t.Error("The expected value for J is:", expectedJ, "but the returned value is:", j)
-    }
-
-    for i := 0; i < len(grad); i++ {
-	if (grad[i] != expectedGrad[i]) {
-	    t.Error("The expected gradient is:", expectedGrad[i], "but the returned value is:", grad[i])
-	}
-    }
-
-    fmt.Println("J:", j)
-    fmt.Println("Grad:", grad)
-    fmt.Println("Err:", err)
 }
