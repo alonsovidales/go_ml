9dd95c150b1d8f9d8ac6afd264927b6099b19159
Alonso Vidales <alonso.vidales@tras2.es>
2013-12-02 23:21:16 +0100

    Added comments, and formatting

--- regression.go
+++ regression.go
@@ -1,191 +1,216 @@
+/*
+Package ml, implements a set of machine learning algorithm for linear regrassion
+
+*/
 package ml
 
 import (
-    "fmt"
-    "math"
-    "io/ioutil"
-    "strings"
-    "strconv"
-    "math/rand"
-    "time"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type DataSet struct {
-    X [][]float64 // Training set of values for each feature
-    Y []float64 // The training set with values to be predicted
+	X [][]float64 // Training set of values for each feature
+	Y []float64   // The training set with values to be predicted
 }
 
 // Calculates the cost function and gradient for the data with the given theta
-// and lambda 
+// and lambda
 func (data *DataSet) LinearRegCostFunction(theta []float64, lambda float64) (j float64, grad []float64, err error) {
-    if len(data.Y) != len(data.X) {
-        err = fmt.Errorf(
-            "The number of test cases (X) %d doesn't corresponds with the number of values (Y) %d",
-            len(data.X),
-            len(data.Y))
-        return
-    }
+	if len(data.Y) != len(data.X) {
+		err = fmt.Errorf(
+			"The number of test cases (X) %d doesn't corresponds with the number of values (Y) %d",
+			len(data.X),
+			len(data.Y))
+		return
+	}
 
-    if len(theta) != len(data.X[0]) {
-        err = fmt.Errorf(
-            "The Theta arg has a lenght of %d and the input data %d",
-            len(theta),
-            len(data.X[0]))
-        return
-    }
+	if len(theta) != len(data.X[0]) {
+		err = fmt.Errorf(
+			"The Theta arg has a lenght of %d and the input data %d",
+			len(theta),
+			len(data.X[0]))
+		return
+	}
 
-    m := len(data.X)
-    mF := float64(m)
-    // Calculate the hipotesis for each training val
-    hipotesis := make([]float64, m)
+	m := len(data.X)
+	mF := float64(m)
+	// Calculate the hipotesis for each training val
+	hipotesis := make([]float64, m)
 
-    for i := 0; i < m; i++ {
-        for j:= 0; j < len(theta); j++ {
-            hipotesis[i] += data.X[i][j] * theta[j]
-        }
-    }
+	for i := 0; i < m; i++ {
+		for j := 0; j < len(theta); j++ {
+			hipotesis[i] += data.X[i][j] * theta[j]
+		}
+	}
 
-    cost := 0.0
+	cost := 0.0
 
-    grad = make([]float64, len(theta))
-    // Calculate the sum of the square of distances between the wanted result
-    // and the actual one, the total cost
-    for i := 0; i < len(hipotesis); i++ {
-        cost += math.Pow(hipotesis[i] - data.Y[i], 2)
+	grad = make([]float64, len(theta))
+	// Calculate the sum of the square of distances between the wanted result
+	// and the actual one, the total cost
+	for i := 0; i < len(hipotesis); i++ {
+		cost += math.Pow(hipotesis[i]-data.Y[i], 2)
 
-        for j:= 0; j < len(theta); j++ {
-            grad[j] += (hipotesis[i] - data.Y[i]) * data.X[i][j]
-        }
-    }
+		for j := 0; j < len(theta); j++ {
+			grad[j] += (hipotesis[i] - data.Y[i]) * data.X[i][j]
+		}
+	}
 
-    thetaReg := 0.0
-    for i := 1; i < len(theta); i++ {
-        thetaReg += math.Pow(theta[i], 2)
-    }
+	thetaReg := 0.0
+	for i := 1; i < len(theta); i++ {
+		thetaReg += math.Pow(theta[i], 2)
+	}
 
-    // Calculate the regularized cost
-    j = (cost / float64(2 * m)) + ((lambda * thetaReg) / float64(2 * m))
+	// Calculate the regularized cost
+	j = (cost / float64(2*m)) + ((lambda * thetaReg) / float64(2*m))
 
-    grad[0] /= mF
-    for j:= 1; j < len(theta); j++ {
-        grad[j] = (grad[j] / mF) + ((lambda * theta[j]) / mF)
-    }
+	grad[0] /= mF
+	for j := 1; j < len(theta); j++ {
+		grad[j] = (grad[j] / mF) + ((lambda * theta[j]) / mF)
+	}
 
-    return
+	return
 }
 
+// For the given set of data, calculates the optimal theta values in order to
+// minimize the cost
+// The alpha param is calculated dinamically on the first iterations, the
+// maxIters param indicates the maximal number of iterations to obtain the
+// optimal solution, in case of detect a difference of the cost between
+// iterations smaller than 0.001 returns the theta without stopping the iteration
 func (data *DataSet) MinimizeTheta(initTheta []float64, lambda float64, maxIters int) (theta []float64) {
-    theta = initTheta
+	theta = initTheta
 
-    // We will try to adjust this alpha to the best possible on the first iterations
-    alpha := 1.0
+	// We will try to adjust this alpha to the best possible on the first iterations
+	alpha := 1.0
 
-    jTraining, _, err := data.LinearRegCostFunction(theta, lambda)
-    if err != nil { panic(err) }
-    lastJ := jTraining + 1
-    lastTheta := make([]float64, len(initTheta))
+	jTraining, _, err := data.LinearRegCostFunction(theta, lambda)
+	if err != nil {
+		panic(err)
+	}
+	lastJ := jTraining + 1
+	lastTheta := make([]float64, len(initTheta))
 
-    for iter := 0; iter < maxIters; iter++ {
-        jTraining, grad, err := data.LinearRegCostFunction(theta, lambda)
-        if err != nil { panic(err) }
+	for iter := 0; iter < maxIters; iter++ {
+		jTraining, grad, err := data.LinearRegCostFunction(theta, lambda)
+		if err != nil {
+			panic(err)
+		}
 
-        if jTraining > lastJ {
-            alpha /= 10
+		if jTraining > lastJ {
+			alpha /= 10
 
-            lastJ = jTraining
-            copy(theta, lastTheta)
-        } else {
-            copy(lastTheta, theta)
-            for j := 0; j < len(theta); j++ {
-                theta[j] -= alpha * grad[j]
-            }
+			lastJ = jTraining
+			copy(theta, lastTheta)
+		} else {
+			copy(lastTheta, theta)
+			for j := 0; j < len(theta); j++ {
+				theta[j] -= alpha * grad[j]
+			}
 
-            if lastJ - jTraining < 0.001 {
-                return
-            }
+			if lastJ-jTraining < 0.001 {
+				return
+			}
 
-            lastJ = jTraining
-        }
-    }
+			lastJ = jTraining
+		}
+	}
 
-    return
+	return
 }
 
+// Returns the given set of data with a random order in order to obtain a good
+// distribution
 func (data *DataSet) shuffle() (shuffledData *DataSet) {
-    aux := make([][]float64, len(data.X))
+	aux := make([][]float64, len(data.X))
 
-    copy(aux, data.X)
-    for i := 0; i < len(aux); i++ {
-        aux[i] = append(aux[i], data.Y[i])
-    }
+	copy(aux, data.X)
+	for i := 0; i < len(aux); i++ {
+		aux[i] = append(aux[i], data.Y[i])
+	}
 
-    dest := make([][]float64, len(aux))
-    rand.Seed(int64(time.Now().Nanosecond()))
-    perm := rand.Perm(len(aux))
-    for i, v := range perm {
-        dest[v] = aux[i]
-    }
+	dest := make([][]float64, len(aux))
+	rand.Seed(int64(time.Now().Nanosecond()))
+	perm := rand.Perm(len(aux))
+	for i, v := range perm {
+		dest[v] = aux[i]
+	}
 
-    shuffledData = &DataSet{
-        X: dest,
-        Y: make([]float64, len(dest)),
-    }
-    for i := 0; i < len(aux); i++ {
-        shuffledData.Y[i] = shuffledData.X[i][len(shuffledData.X[i]) - 1]
-        shuffledData.X[i] = shuffledData.X[i][:len(shuffledData.X[i]) - 1]
-    }
+	shuffledData = &DataSet{
+		X: dest,
+		Y: make([]float64, len(dest)),
+	}
+	for i := 0; i < len(aux); i++ {
+		shuffledData.Y[i] = shuffledData.X[i][len(shuffledData.X[i])-1]
+		shuffledData.X[i] = shuffledData.X[i][:len(shuffledData.X[i])-1]
+	}
 
-    return
+	return
 }
 
+// This metod splits the given data in three sets: training, cross validation,
+// test. In order to calculate the optimal theta, tries with different
+// possibilities and the training data, and check the best match with the cross
+// validations, after obtain the best lambda, check the perfomand against the
+// test set of data
 func (data *DataSet) CalcOptimumLambdaTheta(maxIters int, suffleData bool) (lambda float64, theta []float64, performance float64) {
-    lambdas := []float64{0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10}
+	lambdas := []float64{0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10}
 
-    // Get the 60% of the data as training data, 20% as cross validation, and
-    // the remaining 20% as test data
-    training := int64(float64(len(data.X)) * 0.6)
-    cv := int64(float64(len(data.X)) * 0.8)
+	if suffleData {
+		data = data.shuffle()
+	}
 
-    if suffleData {
-        data = data.shuffle()
-    }
+	// Get the 60% of the data as training data, 20% as cross validation, and
+	// the remaining 20% as test data
+	training := int64(float64(len(data.X)) * 0.6)
+	cv := int64(float64(len(data.X)) * 0.8)
 
-    trainingData := &DataSet{
-        X: data.X[:training],
-        Y: data.Y[:training],
-    }
-    cvData := &DataSet{
-        X: data.X[training:cv],
-        Y: data.Y[training:cv],
-    }
-    testData := &DataSet{
-        X: data.X[cv:],
-        Y: data.Y[cv:],
-    }
+	trainingData := &DataSet{
+		X: data.X[:training],
+		Y: data.Y[:training],
+	}
+	cvData := &DataSet{
+		X: data.X[training:cv],
+		Y: data.Y[training:cv],
+	}
+	testData := &DataSet{
+		X: data.X[cv:],
+		Y: data.Y[cv:],
+	}
 
-    bestLambda := 0.0
-    posTheta := trainingData.MinimizeTheta(make([]float64, len(trainingData.X[0])), bestLambda, maxIters)
-    bestTheta := posTheta
-    jCv, _, err := cvData.LinearRegCostFunction(posTheta, bestLambda)
-    if err != nil { panic(err) }
-    lowerJ := jCv
-    for _, posLambda := range lambdas {
-        posTheta := trainingData.MinimizeTheta(make([]float64, len(trainingData.X[0])), posLambda, maxIters)
-        jCv, _, err := cvData.LinearRegCostFunction(posTheta, 0)
-        if err != nil { panic(err) }
+	bestLambda := 0.0
+	posTheta := trainingData.MinimizeTheta(make([]float64, len(trainingData.X[0])), bestLambda, maxIters)
+	bestTheta := posTheta
+	jCv, _, err := cvData.LinearRegCostFunction(posTheta, bestLambda)
+	if err != nil {
+		panic(err)
+	}
+	lowerJ := jCv
+	for _, posLambda := range lambdas {
+		posTheta := trainingData.MinimizeTheta(make([]float64, len(trainingData.X[0])), posLambda, maxIters)
+		jCv, _, err := cvData.LinearRegCostFunction(posTheta, 0)
+		if err != nil {
+			panic(err)
+		}
 
-        if jCv < lowerJ {
-            bestTheta = posTheta
-            lowerJ = jCv
-            bestLambda = posLambda
-        }
-    }
+		if jCv < lowerJ {
+			bestTheta = posTheta
+			lowerJ = jCv
+			bestLambda = posLambda
+		}
+	}
 
-    lambda = bestLambda
-    theta = bestTheta
-    performance, _, _ = testData.LinearRegCostFunction(theta, 0)
+	lambda = bestLambda
+	theta = bestTheta
+	performance, _, _ = testData.LinearRegCostFunction(theta, 0)
 
-    return
+	return
 }
 
 // Loads information from the local file located at filePath, and after parse
@@ -198,25 +223,29 @@ func (data *DataSet) CalcOptimumLambdaTheta(maxIters int, suffleData bool) (lamb
 //
 // Note: Use a single space as separator
 func LoadFile(filePath string) (data *DataSet) {
-    strInfo, err := ioutil.ReadFile(filePath)
-    if err != nil { panic(err) }
-    data = new(DataSet)
+	strInfo, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	data = new(DataSet)
 
-    trainingData := strings.Split(string(strInfo), "\n")
-    for _, line := range trainingData {
-        if line == "" {
-            break
-        }
+	trainingData := strings.Split(string(strInfo), "\n")
+	for _, line := range trainingData {
+		if line == "" {
+			break
+		}
 
-        var values []float64
-        for _, value := range strings.Split(line, " ") {
-            floatVal, err := strconv.ParseFloat(value, 64)
-            if err != nil { panic(err) }
-            values = append(values, floatVal)
-        }
-        data.X = append(data.X, values[:len(values) - 1])
-        data.Y = append(data.Y, values[len(values) - 1])
-    }
+		var values []float64
+		for _, value := range strings.Split(line, " ") {
+			floatVal, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				panic(err)
+			}
+			values = append(values, floatVal)
+		}
+		data.X = append(data.X, values[:len(values)-1])
+		data.Y = append(data.Y, values[len(values)-1])
+	}
 
-    return
+	return
 }
--- regression_test.go
+++ regression_test.go
@@ -1,90 +1,90 @@
 package ml
 
 import (
-    "testing"
+	"testing"
 )
 
 // Using a predefined dataset stored in test_data/test_logistic.dat , calculate
 // the cost and gradient for different lambda y theta
 func TestLinearRegCostFunction(t *testing.T) {
 
-    // Data obtained from the "Andrew Ng" machine learning course from Coursera
-    // https://www.coursera.org/course/ml
-    data := LoadFile("test_data/test_logistic.dat")
+	// Data obtained from the "Andrew Ng" machine learning course from Coursera
+	// https://www.coursera.org/course/ml
+	data := LoadFile("test_data/test_logistic.dat")
 
-    theta := [][]float64 {
-        []float64{1, 1},
-        []float64{13.08790, 0.36778},
-    }
-    lambda := []float64 {
-        1,
-        0,
-    }
+	theta := [][]float64{
+		[]float64{1, 1},
+		[]float64{13.08790, 0.36778},
+	}
+	lambda := []float64{
+		1,
+		0,
+	}
 
-    expectedJ := []float64 {
-        303.9937949908333,
-        22.3738964254415,
-    }
+	expectedJ := []float64{
+		303.9937949908333,
+		22.3738964254415,
+	}
 
-    expectedGrad := [][]float64 {
-        []float64{-15.303049999999999, 598.251293035},
-        []float64{-3.2069500000601416e-05, 0.0009341162152152194},
-    }
+	expectedGrad := [][]float64{
+		[]float64{-15.303049999999999, 598.251293035},
+		[]float64{-3.2069500000601416e-05, 0.0009341162152152194},
+	}
 
-    for test := 0; test < len(expectedGrad); test++ {
-        j, grad, err := data.LinearRegCostFunction(theta[test], lambda[test])
+	for test := 0; test < len(expectedGrad); test++ {
+		j, grad, err := data.LinearRegCostFunction(theta[test], lambda[test])
 
-        // The expected results are:
-        //        J: 303.99
-        //        Grad: [-15.303, 598.251]
+		// The expected results are:
+		//        J: 303.99
+		//        Grad: [-15.303, 598.251]
 
-        if err != nil {
-            t.Error("The LinearRegCostFunction returned an error:", err)
-        }
+		if err != nil {
+			t.Error("The LinearRegCostFunction returned an error:", err)
+		}
 
-        if (j != expectedJ[test]) {
-            t.Error("The expected value for J is:", expectedJ[test], "but the returned value is:", j)
-        }
+		if j != expectedJ[test] {
+			t.Error("The expected value for J is:", expectedJ[test], "but the returned value is:", j)
+		}
 
-        for i := 0; i < len(grad); i++ {
-            if (grad[i] != expectedGrad[test][i]) {
-                t.Error("The expected gradient is:", expectedGrad[test][i], "but the returned value is:", grad[i])
-            }
-        }
-    }
+		for i := 0; i < len(grad); i++ {
+			if grad[i] != expectedGrad[test][i] {
+				t.Error("The expected gradient is:", expectedGrad[test][i], "but the returned value is:", grad[i])
+			}
+		}
+	}
 }
 
 func TestCalculateOptimumData(t *testing.T) {
 
-    // Data obtained from the "Andrew Ng" machine learning course from Coursera
-    // https://www.coursera.org/course/ml
-    data := LoadFile("test_data/test_logistic_polynomial.dat")
+	// Data obtained from the "Andrew Ng" machine learning course from Coursera
+	// https://www.coursera.org/course/ml
+	data := LoadFile("test_data/test_logistic_polynomial.dat")
 
-    lambda, theta, performance := data.CalcOptimumLambdaTheta(2000, false)
+	lambda, theta, performance := data.CalcOptimumLambdaTheta(2000, false)
 
-    if (performance != 4.720931077222739) {
-        t.Error("The expected performance is: 4.720931077222739 but the returned value is:", performance)
-    }
+	if performance != 4.720931077222739 {
+		t.Error("The expected performance is: 4.720931077222739 but the returned value is:", performance)
+	}
 
-    if (lambda != 0.01) {
-        t.Error("The expected lambda is: 0.1 but the returned value is:", lambda)
-    }
+	if lambda != 0.01 {
+		t.Error("The expected lambda is: 0.1 but the returned value is:", lambda)
+	}
 
-    expectedTheta := []float64{
-        11.600497233712602,
-        8.457034058625915,
-        5.121089248288871,
-        3.702976145326119,
-        2.6521200466209134,
-        1.4601346022472674,
-        0.7517304315857527,
-        0.2385277594908083,
-        -0.3243681654026378,
-    }
+	expectedTheta := []float64{
+		11.600497233712602,
+		8.457034058625915,
+		5.121089248288871,
+		3.702976145326119,
+		2.6521200466209134,
+		1.4601346022472674,
+		0.7517304315857527,
+		0.2385277594908083,
+		-0.3243681654026378,
+	}
 
-    for i := 0; i < len(expectedTheta); i++ {
-        if (theta[i] != expectedTheta[i]) {
-            t.Error("The expected theta value on", i, " is:", expectedTheta[i], "but the returned value was:", theta[i])
-        }
-    }
+	for i := 0; i < len(expectedTheta); i++ {
+		if theta[i] != expectedTheta[i] {
+			t.Error("The expected theta value on", i, " is:", expectedTheta[i], "but the returned value was:", theta[i])
+		}
+	}
 }
