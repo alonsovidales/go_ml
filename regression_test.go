25386e41120449dcde5b3e6c3031a65a0ab151ec
Alonso Vidales <alonso.vidales@tras2.es>
2013-12-22 18:55:34 +0100

    Fixed problem with test files names

--- regression_test.go
+++ regression_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-// Using a predefined dataset stored in test_data/test_logistic.dat , calculate
+// Using a predefined dataset stored in test_data/test_linear.dat , calculate
 // the cost and gradient for different lambda y theta
 func TestLinearRegCostFunction(t *testing.T) {
 
 	// Data obtained from the "Andrew Ng" machine learning course from Coursera
 	// https://www.coursera.org/course/ml
-	data := LoadFile("test_data/test_logistic.dat", true)
+	data := LoadFile("test_data/test_linear.dat", true)
 
 	theta := [][]float64{
 		[]float64{1, 1},
@@ -54,11 +54,46 @@ func TestLinearRegCostFunction(t *testing.T) {
 	}
 }
 
-func TestCalculateOptimumData(t *testing.T) {
+func TestCalculateOptimumDataLinearReg(t *testing.T) {
 
 	// Data obtained from the "Andrew Ng" machine learning course from Coursera
 	// https://www.coursera.org/course/ml
-	data := LoadFile("test_data/test_logistic_polynomial.dat", true)
+	data := LoadFile("test_data/test_linear_polynomial.dat", true)
+
+	lambda, theta, performance := data.CalcOptimumLambdaTheta(2000, false)
+
+	if performance != 4.901045349988667 {
+		t.Error("The expected performance is: 4.901045349988667 but the returned value is:", performance)
+	}
+
+	if lambda != 0.1 {
+		t.Error("The expected lambda is: 0.1 but the returned value is:", lambda)
+	}
+
+	expectedTheta := []float64{
+		11.668527097769664,
+		9.227365853102674,
+		5.901606532678573,
+		2.732609803669128,
+		2.048572624793663,
+		1.417634232412458,
+		-0.23465057648197382,
+		0.5051641750629966,
+		0.43653605626382835,
+	}
+
+	for i := 0; i < len(expectedTheta); i++ {
+		if theta[i] != expectedTheta[i] {
+			t.Error("The expected theta value on", i, " is:", expectedTheta[i], "but the returned value was:", theta[i])
+		}
+	}
+}
+
+func TestCalculateOptimumDataLogReg(t *testing.T) {
+
+	// Data obtained from the "Andrew Ng" machine learning course from Coursera
+	// https://www.coursera.org/course/ml
+	data := LoadFile("test_data/test_linear_polynomial.dat", true)
 
 	lambda, theta, performance := data.CalcOptimumLambdaTheta(2000, false)
 
